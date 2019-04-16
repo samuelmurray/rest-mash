01a118ba820fc79a2a2c0d713c644fa3dddf04e4
Samuel Murray <samuelmu@kth.se>
2019-04-16 14:26:29 +0200

    Add field id

--- src/main/java/app/Album.java
+++ src/main/java/app/Album.java
@@ -2,6 +2,7 @@ package app;
 
 public class Album {
     private String title;
+    private String id;
 
     public Album() {
     }
@@ -12,5 +13,13 @@ public class Album {
 
     public String getTitle() {
         return title;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
     }
 }
