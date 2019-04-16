5d4e937d997dcd51dd0a3074786084902d6dfb13
Samuel Murray <samuelmu@kth.se>
2019-04-16 14:20:52 +0200

    Initial commit: Album.java

---
+++ src/main/java/app/Album.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Album {
+    private String title;
+
+    public Album() {
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
