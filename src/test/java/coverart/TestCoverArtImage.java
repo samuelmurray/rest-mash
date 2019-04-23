9799051d87eb8afbfd020c11a539b2f9f5920c91
Samuel Murray <samuelmu@kth.se>
2019-04-23 17:54:54 +0200

    Add testGetUrlNull

--- src/test/java/coverart/TestCoverArtImage.java
+++ src/test/java/coverart/TestCoverArtImage.java
@@ -3,8 +3,16 @@ package coverart;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TestCoverArtImage {
+
+    @Test
+    public void testGetUrlNull() {
+        CoverArtImage image = new CoverArtImage();
+        assertNull(image.getUrl());
+    }
+
     @Test
     public void testGetUrl() {
         CoverArtImage image = new CoverArtImage();
