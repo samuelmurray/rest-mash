fb230e444121989dab8da0a36a19723dfe1a3599
Samuel Murray <samuelmu@kth.se>
2019-04-23 18:02:05 +0200

    Add tests for query getter and setter

--- src/test/java/wikipedia/TestWikipediaContent.java
+++ src/test/java/wikipedia/TestWikipediaContent.java
@@ -3,9 +3,11 @@ package wikipedia;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TestWikipediaContent {
 
@@ -23,5 +25,20 @@ public class TestWikipediaContent {
         String title = "Samu Ruotsalainen";
         WikipediaContent content = WikipediaContentFactory.createFromWikipediaTitle(title);
         Assertions.assertThrows(NoSuchElementException.class, content::getExtract);
+    }
+
+    @Test
+    public void testGetQueryNull() {
+        WikipediaContent content = new WikipediaContent();
+        assertNull(content.getQuery());
+    }
+
+    @Test
+    public void testGetQuery() {
+        WikipediaContent content = new WikipediaContent();
+        Map<String, Object> query = new HashMap<>();
+        query.put("Key", null);
+        content.setQuery(query);
+        assertEquals(query, content.getQuery());
     }
 }
