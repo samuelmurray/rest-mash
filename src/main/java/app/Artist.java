a27397dbe2bc0c3da464789f42b1187f8c7dfb61
Samuel Murray <samuelmu@kth.se>
2019-04-23 10:49:10 +0200

    Add attribute wikiUrl and initialize it in constructor

--- src/main/java/app/Artist.java
+++ src/main/java/app/Artist.java
@@ -3,16 +3,22 @@ package app;
 public class Artist {
     private final long id;
     private final MusicBrainzContent content;
+    private final String wikiUrl;
 
     public Artist(long id, String mbid) {
         this.id = id;
         MusicBrainzConsumer consumer = new MusicBrainzConsumer(mbid);
         content = consumer.getContent();
         // content.addCoverArtToAlbums();  // FIXME: Remove before merging
+        wikiUrl = content.getWikiUrl();
     }
 
     public long getId() {
         return id;
+    }
+
+    public String getWikiUrl() {
+        return wikiUrl;
     }
 
     public MusicBrainzContent getContent() {
