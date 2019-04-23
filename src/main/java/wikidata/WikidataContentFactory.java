52f095b1c97303ca9618f1fc7e176a23a722498f
Samuel Murray <samuelmu@kth.se>
2019-04-23 14:58:58 +0200

    Remove unused code

--- src/main/java/wikidata/WikidataContentFactory.java
+++ src/main/java/wikidata/WikidataContentFactory.java
@@ -5,13 +5,8 @@ import org.springframework.web.client.RestTemplate;
 public class WikidataContentFactory {
     private static final String urlPrefix = "https://www.wikidata.org/w/api.php?action=wbgetentities&ids=";
     private static final String urlSuffix = "&format=json&props=sitelinks";
-    private RestTemplate restTemplate;
-    private WikidataContent content;
 
-
-    public WikidataContentFactory(String wikidataId) {
-        restTemplate = new RestTemplate();
-        consume(wikidataId);
+    private WikidataContentFactory() {
     }
 
     public static WikidataContent createFromWikidataId(String wikidataId) {
@@ -19,17 +14,7 @@ public class WikidataContentFactory {
         return restTemplate.getForObject(buildUrl(wikidataId), WikidataContent.class);
     }
 
-
-    private void consume(String wikidataId) {
-        content = restTemplate.getForObject(buildUrl(wikidataId), WikidataContent.class);
-    }
-
-    public WikidataContent getContent() {
-        return content;
-    }
-
     private static String buildUrl(String wikidataId) {
         return urlPrefix + wikidataId + urlSuffix;
     }
 }
-
