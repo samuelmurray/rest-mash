0d83a08381a04dee956408f44447de62f0998c7e
Samuel Murray <samuelmu@kth.se>
2019-04-23 14:52:08 +0200

    Remove unused code

--- src/main/java/musicbrainz/MusicBrainzConsumer.java
+++ src/main/java/musicbrainz/MusicBrainzConsumer.java
@@ -5,25 +5,13 @@ import org.springframework.web.client.RestTemplate;
 public class MusicBrainzConsumer {
     private static final String urlPrefix = "http://musicbrainz.org/ws/2/artist/";
     private static final String urlSuffix = "?&fmt=json&inc=url-rels+release-groups";
-    private RestTemplate restTemplate;
-    private MusicBrainzContent content;
 
-    public MusicBrainzConsumer(String mbid) {
-        restTemplate = new RestTemplate();
-        consume(mbid);
+    private MusicBrainzConsumer(String mbid) {
     }
 
     public static MusicBrainzContent createFromMbid(String mbid) {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(buildUrl(mbid), MusicBrainzContent.class);
-    }
-
-    private void consume(String mbid) {
-        content = restTemplate.getForObject(buildUrl(mbid), MusicBrainzContent.class);
-    }
-
-    public MusicBrainzContent getContent() {
-        return content;
     }
 
     private static String buildUrl(String mbid) {
