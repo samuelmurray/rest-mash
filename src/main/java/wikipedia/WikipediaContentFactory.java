073984327706c7cf9afed25d4da26577c1a9e1b7
Samuel Murray <samuelmu@kth.se>
2019-04-23 15:04:28 +0200

    Remove unused code

--- src/main/java/wikipedia/WikipediaContentFactory.java
+++ src/main/java/wikipedia/WikipediaContentFactory.java
@@ -4,28 +4,14 @@ import org.springframework.web.client.RestTemplate;
 
 public class WikipediaContentFactory {
     private static final String urlPrefix = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=true&redirects=true&titles=";
-    private RestTemplate restTemplate;
-    private WikipediaContent content;
 
-
-    public WikipediaContentFactory(String wikipediaTitle) {
-        restTemplate = new RestTemplate();
-        consume(wikipediaTitle);
+    private WikipediaContentFactory() {
     }
 
     public static WikipediaContent createFromWikipediaTitle(String wikipediaTitle) {
         RestTemplate restTemplate = new RestTemplate();
         String url = buildUrl(wikipediaTitle);
         return restTemplate.getForObject(url, WikipediaContent.class);
-    }
-
-    private void consume(String wikipediaTitle) {
-        String url = buildUrl(wikipediaTitle);
-        content = restTemplate.getForObject(url, WikipediaContent.class);
-    }
-
-    public WikipediaContent getContent() {
-        return content;
     }
 
     private static String buildUrl(String wikipediaTitle) {
