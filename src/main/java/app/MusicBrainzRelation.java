c1c511507801f1e0f65cffa8a655b561923f82fd
Samuel Murray <samuelmu@kth.se>
2019-04-23 10:23:24 +0200

    Add private field url

--- src/main/java/app/MusicBrainzRelation.java
+++ src/main/java/app/MusicBrainzRelation.java
@@ -1,11 +1,19 @@
 package app;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MusicBrainzRelation {
     private String type;
+    private MusicBrainzUrl url;
+
+    public MusicBrainzUrl getUrl() {
+        return url;
+    }
+
+    public void setUrl(MusicBrainzUrl url) {
+        this.url = url;
+    }
 
     public String getType() {
         return type;
