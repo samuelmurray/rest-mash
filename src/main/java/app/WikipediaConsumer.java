360e52c7e50cb28b3e653dc415dfb3bbf2217f10
Samuel Murray <samuelmu@kth.se>
2019-04-23 12:43:51 +0200

    Remove unused imports

--- src/main/java/app/WikipediaConsumer.java
+++ src/main/java/app/WikipediaConsumer.java
@@ -2,12 +2,6 @@ package app;
 
 import org.springframework.web.client.RestTemplate;
 
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-
 public class WikipediaConsumer {
     private static final String urlPrefix = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=true&redirects=true&titles=";
     private RestTemplate restTemplate;
