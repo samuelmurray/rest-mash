c475c48f3eb319ac70a38c3e8a251e96e2a63096
Samuel Murray <samuelmu@kth.se>
2019-04-23 12:05:24 +0200

    Use WikipediaConsumer to create WikipediaContent

--- src/main/java/app/Artist.java
+++ src/main/java/app/Artist.java
@@ -4,17 +4,21 @@ public class Artist {
     private final long id;
     private final MusicBrainzContent content;
     private final WikidataContent wikidataContent;
+    private final WikipediaContent wikipediaContent;
     private final String enwikiTitle;
 
     public Artist(long id, String mbid) {
         this.id = id;
         MusicBrainzConsumer consumer = new MusicBrainzConsumer(mbid);
         content = consumer.getContent();
-        content.addCoverArtToAlbums();
+        // content.addCoverArtToAlbums();  // FIXME: Add before merge
         String wikidataId = content.getWikiDataId();
         WikidataConsumer wikiDataConsumer = new WikidataConsumer(wikidataId);
         wikidataContent = wikiDataConsumer.getContent();
         enwikiTitle = wikidataContent.getEnwikiTitle(wikidataId);
+        WikipediaConsumer wikipediaConsumer = new WikipediaConsumer(enwikiTitle);
+        wikipediaContent = wikipediaConsumer.getContent();
+        System.out.println(wikipediaContent.getQuery());
     }
 
     public long getId() {
