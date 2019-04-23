93abba4855534ea5ffdef3bbdbf4d37f7357c26d
Samuel Murray <samuelmu@kth.se>
2019-04-23 10:47:35 +0200

    Add method lastPartOfUrl

--- src/main/java/app/MusicBrainzUrl.java
+++ src/main/java/app/MusicBrainzUrl.java
@@ -2,6 +2,9 @@ package app;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MusicBrainzUrl {
     private String resource;
@@ -12,5 +15,12 @@ public class MusicBrainzUrl {
 
     public void setResource(String resource) {
         this.resource = resource;
+    }
+
+    public String lastPartOfUrl() throws URISyntaxException {
+        URI uri = new URI(getResource());
+        System.out.println(uri.toString());
+        String[] segments = uri.getPath().split("/");
+        return segments[segments.length - 1];
     }
 }
