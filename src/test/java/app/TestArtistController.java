5379426d3c1afb345763679e91f4f7db522b3bab
Samuel Murray <samuelmu@kth.se>
2019-04-24 15:21:31 +0200

    Add test for ArstistController

---
+++ src/test/java/app/TestArtistController.java
@@ -0,0 +1,16 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestArtistController {
+
+    @Test
+    void testArtist() {
+        String mbid = "09302d63-d57f-4fe0-9c37-095843b91dff";
+        ArtistController controller = new ArtistController();
+        Artist artist = controller.artist(mbid);
+        assertEquals(mbid, artist.getMbid());
+    }
+}
