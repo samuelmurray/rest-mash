54cb55c9ec043daf22a830752a4c62f9aa5fbeec
Samuel Murray <samuelmu@kth.se>
2019-04-24 14:49:17 +0200

    Add tests for MusicBrainzContentFactory

--- src/test/java/musicbrainz/TestMusicBrainzContent.java
+++ src/test/java/musicbrainz/TestMusicBrainzContent.java
@@ -3,11 +3,60 @@ package musicbrainz;
 import app.Album;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestMusicBrainzContent {
+
+    @Test
+    void testContentFactoryName() {
+        String mbid = "5b11f4ce-a62d-471e-81fc-a69a8278c7da";
+        MusicBrainzContent content = MusicBrainzContentFactory.createFromMbid(mbid);
+        String expected = "Nirvana";
+        assertEquals(expected, content.getName());
+    }
+
+    @Test
+    void testContentFactoryWikipediaTitleMissing() {
+        String mbid = "5b11f4ce-a62d-471e-81fc-a69a8278c7da";
+        MusicBrainzContent content = MusicBrainzContentFactory.createFromMbid(mbid);
+        assertThrows(NoSuchElementException.class, content::getWikipediaTitle);
+    }
+
+    @Test
+    void testContentFactoryWikipediaTitle() {
+        String mbid = "a04e7da2-998b-4e36-abaa-014c00b93622";
+        MusicBrainzContent content = MusicBrainzContentFactory.createFromMbid(mbid);
+        String expected = "Veronica_Maggio";
+        try {
+            assertEquals(expected, content.getWikipediaTitle());
+        } catch (URISyntaxException e) {
+            fail();
+        }
+    }
+
+    @Test
+    void testContentFactoryWikidataId() {
+        String mbid = "a04e7da2-998b-4e36-abaa-014c00b93622";
+        MusicBrainzContent content = MusicBrainzContentFactory.createFromMbid(mbid);
+        String expected = "Q260597";
+        try {
+            assertEquals(expected, content.getWikidataId());
+        } catch (URISyntaxException e) {
+            fail();
+        }
+    }
+
+    @Test
+    void testContentFactoryWikidataIdMissing() {
+        String mbid = "09302d63-d57f-4fe0-9c37-095843b91dff";
+        MusicBrainzContent content = MusicBrainzContentFactory.createFromMbid(mbid);
+        assertThrows(NoSuchElementException.class, content::getWikidataId);
+    }
 
     @Test
     void testGetAlbums() {
