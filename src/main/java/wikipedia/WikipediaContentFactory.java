2f2e74149e75e5b81c6b6651f188ce83e118a081
Samuel Murray <samuelmu@kth.se>
2019-04-25 10:08:04 +0200

    Add method createFromWikidataId

--- src/main/java/wikipedia/WikipediaContentFactory.java
+++ src/main/java/wikipedia/WikipediaContentFactory.java
@@ -1,11 +1,19 @@
 package wikipedia;
 
 import org.springframework.web.client.RestTemplate;
+import wikidata.WikidataContent;
+import wikidata.WikidataContentFactory;
 
 public class WikipediaContentFactory {
     private static final String URL_PREFIX = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=true&redirects=true&titles=";
 
     private WikipediaContentFactory() {
+    }
+
+    public static WikipediaContent createFromWikidataId(String wikidataId) {
+        WikidataContent content = WikidataContentFactory.createFromWikidataId(wikidataId);
+        String wikipediaTitle = content.getEnwikiTitle(wikidataId);
+        return createFromWikipediaTitle(wikipediaTitle);
     }
 
     public static WikipediaContent createFromWikipediaTitle(String wikipediaTitle) {
