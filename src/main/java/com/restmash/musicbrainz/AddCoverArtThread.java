3d3edd9edbd25126a06890bbb2a33710ad0512ae
Samuel Murray <samuelmu@kth.se>
2019-04-26 10:55:08 +0200

    Add method run

--- src/main/java/com/restmash/musicbrainz/AddCoverArtThread.java
+++ src/main/java/com/restmash/musicbrainz/AddCoverArtThread.java
@@ -4,7 +4,12 @@ import com.restmash.app.Album;
 
 public class AddCoverArtThread extends Thread {
     private final Album album;
+
     public AddCoverArtThread(Album album) {
         this.album = album;
+    }
+
+    public void run() {
+        album.addCoverArt();
     }
 }
