0541ccf919d864e244de8beb71b1384280adc5cb
Samuel Murray <samuelmu@kth.se>
2019-04-26 10:38:56 +0200

    Add DI for WikipediaContent through constructor

--- src/main/java/com/restmash/app/Artist.java
+++ src/main/java/com/restmash/app/Artist.java
@@ -1,22 +1,17 @@
 package com.restmash.app;
 
-import com.restmash.musicbrainz.MusicBrainzContentFactory;
 import com.restmash.musicbrainz.MusicBrainzContent;
-import com.restmash.wikipedia.WikipediaContentFactory;
 import com.restmash.wikipedia.WikipediaContent;
-
-import java.net.URISyntaxException;
-import java.util.NoSuchElementException;
 
 public class Artist {
     private final String mbid;
     private final MusicBrainzContent musicBrainzContent;
     private final WikipediaContent wikipediaContent;
 
-    public Artist(String mbid, MusicBrainzContent musicBrainzContent) {
+    public Artist(String mbid, MusicBrainzContent musicBrainzContent, WikipediaContent wikipediaContent) {
         this.mbid = mbid;
         this.musicBrainzContent = musicBrainzContent;
-        wikipediaContent = createWikipediaContent();
+        this.wikipediaContent = wikipediaContent;
     }
 
     public String getMbid() {
@@ -36,32 +31,5 @@ public class Artist {
             return "";
         }
         return wikipediaContent.getExtract();
-    }
-
-    private WikipediaContent createWikipediaContent() {
-        try {
-            return createWikipediaContentSafe();
-        } catch (NoSuchElementException | URISyntaxException e) {
-            System.err.println(String.format("WikipediaContent not created due to Exception: %s", e));
-            return null;
-        }
-    }
-
-    private WikipediaContent createWikipediaContentSafe() throws URISyntaxException {
-        try {
-            return createWikipediaContentFromTitle();
-        } catch (NoSuchElementException e) {
-            return createWikipediaContentFromId();
-        }
-    }
-
-    private WikipediaContent createWikipediaContentFromTitle() throws URISyntaxException {
-        String title = musicBrainzContent.getWikipediaTitle();
-        return WikipediaContentFactory.createFromWikipediaTitle(title);
-    }
-
-    private WikipediaContent createWikipediaContentFromId() throws URISyntaxException {
-        String wikidataId = musicBrainzContent.getWikidataId();
-        return WikipediaContentFactory.createFromWikidataId(wikidataId);
     }
 }
--- src/main/java/com/restmash/app/ArtistController.java
+++ src/main/java/com/restmash/app/ArtistController.java
@@ -2,18 +2,50 @@ package com.restmash.app;
 
 import com.restmash.musicbrainz.MusicBrainzContent;
 import com.restmash.musicbrainz.MusicBrainzContentFactory;
+import com.restmash.wikipedia.WikipediaContent;
+import com.restmash.wikipedia.WikipediaContentFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
 
 @RestController
 public class ArtistController {
 
     @RequestMapping("/artist")
     public Artist artist(@RequestParam(value = "mbid") String mbid) {
-        MusicBrainzContent content = MusicBrainzContentFactory.createFromMbid(mbid);
-        content.addCoverArtToAlbums();
-        Artist artist = new Artist(mbid, content);
-        return artist;
+        MusicBrainzContent musicBrainzContent = MusicBrainzContentFactory.createFromMbid(mbid);
+        musicBrainzContent.addCoverArtToAlbums();
+        WikipediaContent wikipediaContent = createWikipediaContent();
+        return new Artist(mbid, musicBrainzContent, wikipediaContent);
+    }
+
+    private WikipediaContent createWikipediaContent() {
+        try {
+            return createWikipediaContentSafe();
+        } catch (NoSuchElementException | URISyntaxException e) {
+            System.err.println(String.format("WikipediaContent not created due to Exception: %s", e));
+            return null;
+        }
+    }
+
+    private WikipediaContent createWikipediaContentSafe() throws URISyntaxException {
+        try {
+            return createWikipediaContentFromTitle();
+        } catch (NoSuchElementException e) {
+            return createWikipediaContentFromId();
+        }
+    }
+
+    private WikipediaContent createWikipediaContentFromTitle() throws URISyntaxException {
+        String title = musicBrainzContent.getWikipediaTitle();
+        return WikipediaContentFactory.createFromWikipediaTitle(title);
+    }
+
+    private WikipediaContent createWikipediaContentFromId() throws URISyntaxException {
+        String wikidataId = musicBrainzContent.getWikidataId();
+        return WikipediaContentFactory.createFromWikidataId(wikidataId);
     }
 }
