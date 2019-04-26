1c0a2b4257bf2a43af2fce4e4279fa21e103adda
Samuel Murray <samuelmu@kth.se>
2019-04-26 14:24:00 +0200

    Make restTemplate static object instead of local variable

--- src/main/java/com/restmash/musicbrainz/MusicBrainzContentFactory.java
+++ src/main/java/com/restmash/musicbrainz/MusicBrainzContentFactory.java
@@ -5,12 +5,12 @@ import org.springframework.web.client.RestTemplate;
 public class MusicBrainzContentFactory {
     private static final String URL_PREFIX = "http://musicbrainz.org/ws/2/artist/";
     private static final String URL_SUFFIX = "?&fmt=json&inc=url-rels+release-groups";
+    private static final RestTemplate restTemplate = new RestTemplate();
 
     private MusicBrainzContentFactory() {
     }
 
     public static MusicBrainzContent createFromMbid(String mbid) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = buildUrl(mbid);
         return restTemplate.getForObject(url, MusicBrainzContent.class);
     }
--- src/main/java/com/restmash/wikipedia/WikipediaContentFactory.java
+++ src/main/java/com/restmash/wikipedia/WikipediaContentFactory.java
@@ -7,12 +7,12 @@ import org.springframework.web.client.RestTemplate;
 
 public class WikipediaContentFactory {
     private static final String URL_PREFIX = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=true&redirects=true&titles=";
+    private static final RestTemplate restTemplate = new RestTemplate();
 
     private WikipediaContentFactory() {
     }
 
     public static WikipediaContent createFromWikipediaTitle(String wikipediaTitle) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = buildUrl(wikipediaTitle);
         return restTemplate.getForObject(url, WikipediaContent.class);
     }
