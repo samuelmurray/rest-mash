936fefdfb7defcb1aff1d907487e262514940049
Samuel Murray <samuelmu@kth.se>
2019-04-29 11:33:45 +0200

    Remove unused code

--- src/main/java/com/restmash/app/ArtistController.java
+++ src/main/java/com/restmash/app/ArtistController.java
@@ -52,31 +52,4 @@ public class ArtistController {
             e.printStackTrace();
         }
     }
-
-    private WikipediaContent createWikipediaContent() {
-        try {
-            return createWikipediaContentSafe();
-        } catch (NoSuchElementException | URISyntaxException e) {
-            System.err.println(String.format("WikipediaContent not created due to Exception: %s", e));
-            return null;
-        }
-    }
-
-    private WikipediaContent createWikipediaContentSafe() throws URISyntaxException {
-        try {
-            return createWikipediaContentFromTitle();
-        } catch (NoSuchElementException e) {
-            return createWikipediaContentFromId();
-        }
-    }
-
-    private WikipediaContent createWikipediaContentFromTitle() throws URISyntaxException {
-        String title = musicBrainzContent.getWikipediaTitle();
-        return WikipediaContentFactory.createFromWikipediaTitle(title);
-    }
-
-    private WikipediaContent createWikipediaContentFromId() throws URISyntaxException {
-        String wikidataId = musicBrainzContent.getWikidataId();
-        return WikipediaContentFactory.createFromWikidataId(wikidataId);
-    }
 }
